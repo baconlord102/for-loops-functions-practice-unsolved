be0b62eb12c22535a424e5bff1ac1604363bacfd
Gabriel Primus <122728705+baconlord102@users.noreply.github.com>
2025-09-16 13:28:22 -0400

    update

--- exercises/e20.js
+++ exercises/e20.js
@@ -6,8 +6,28 @@
 // NOTE: You MUST use double/nested FOR loop to solve this exercise. The array.includes() method is NOT allowed.
 
 export function separateNamesWithAFromRest(array) {
-  // Your code goes here...
+  let withA = [];
+  let withoutA = [];
 
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i];
+    let hasA = false;
+
+    for (let j = 0; j < name.length; j++) {
+      if (name[j].toLowerCase() === 'a') {
+        hasA = true;
+        break;
+      }
+    }
+
+    if (hasA) {
+      withA.push(name);
+    } else {
+      withoutA.push(name);
+    }
+  }
+
+  return [withA, withoutA];
 }
 
 
